94772dcd23e1c114c68f6b669cc2125f71d79093
Ultimate Raider BTW <dezraymartin@gmail.com>
2023-04-30 17:03:08 -0400

    V 0.1.2

---
+++ Project1/StorePriceApp.cpp
@@ -0,0 +1,18 @@
+/*
+    This is a final project for ITCS 2550 V1601 C++2
+
+    The Authors are:
+        Group Leader: Destin Mingledoff
+        Programmer: Silas Johnson
+        Programmer: Andrew Majewski
+
+    This program will be a store compare application that will take in a JSON file to obtain prices and
+    caclulate which is the cheapest or best value among the stores.
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
