8d6dc1cefdc9f3b1f3d01b36de50a63a091edae4
Ultimate Raider BTW <dezraymartin@gmail.com>
2023-05-06 02:03:25 -0400

    V 0.1.4

--- Project1/StorePriceApp.cpp
+++ Project1/StorePriceApp.cpp
@@ -15,6 +15,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
+#include <fstream>
 
 #include "json.hpp"
 
@@ -35,7 +37,8 @@ struct Items
 // Original Values for Items:
     int id;
     string itemName;
-    Store store;
+    vector<Store> Stores;
+
     // Data not from the json file
     string bestStoreValue;
 
@@ -45,8 +48,6 @@ struct Items
     string bestStoreFound = "";
 };
 
-
-
 // Difference between struct store and unique store.
 // - 3 items, walmart, kroger, and whatever the other one was.
 // - the best store ranking is dependent on whichever has the best price value overall for all accountable items (omitting duplicates).. 
@@ -55,65 +56,97 @@ struct uniqueStore
 {
     string name;
     int score;
-    bool bestStoreValue;
+    bool StorePlaceRank;
 };
 
 
 // Silas Johnson
-template <class T>
+// template <class T>
  class DataExtractor
 {
 public:
     vector<Items> items;
-    vector<uniqueStore> stores;
+    vector<uniqueStore> uStores;
+
+    DataExtractor()
+    {
+        items;
+        uStores;
+    }
+
+
 protected:
-    
     json j;
-
    
-    void getJSONData(T)
-    {
-        file.open(T);
-        Data(T);
-    };
+	void getJSONData(string filename)
+	{
+		ifstream inFile(DATA_FILE + filename);
+		string data;
+		if (inFile.is_open())
+		{
+			while (getline(inFile, data))
+			{
+				j += json::parse(data);
+			}
+		}
+	};
 
-    void parseJSONData()
-    {
-		for (auto& c : j["id"])
-			Items.push_back(c);
+    
 
-        for (auto& c : j["item"])
-			Items.push_back(c);
-
-		for (auto& c : j["name"])
-			Items.push_back(c);
-
-		for (auto& c : j["price"])
-			Items.push_back(c);
+	void parseJSONData()
+	{
+        //items[0].Stores
+		for (auto& jei : j)
+		{
+            
+			Items i;
+			i.id = jei["id"];
+			i.itemName = jei["item"];
+			for (const auto& jes : j["stores"])
+			{
+				Items::Store s;
+				s.name = jes["store"]["name"];
+				s.price = jes["store"]["name"];
+				i.Stores.push_back(s);
+			}
+			items.push_back(i);
+		}
 
 	};
 
-    void Data(file)
-    {
-
-		T >> data;
-		j += data;
-    }
+    
 
     // Andrew Majewksi
-    // This will extract the uninue names form items. 
+    // This will extract the uninue names from items. 
     void extractUniqueStore()
     {
-        while (auto& [key, value] : j.items())
-        {
-            if (key == store)
-        }
-    };
+        unordered_set<string> uniqueStoreNames;
 
+        for (const auto& j["stores"] : store)
+        {
+            const std::string& storeName = item.store.name;
+
+            // Check if the store name is already in the set
+            if (uniqueStoreNames.find(storeName) == uniqueStoreNames.end())
+            {
+                // Add the store name to the set
+                uniqueStoreNames.insert(storeName);
+
+                // Create a new uniqueStore object
+                uniqueStore uniqueStoreObj;
+                uniqueStoreObj.name = storeName;
+                uniqueStoreObj.score = 0;
+                uniqueStoreObj.StorePlaceRank = "";
+
+                // Add the uniqueStore object to the stores vector
+                stores.push_back(uniqueStoreObj);
+            }
+        }
+    }
 
 };
 
-class CalulateData : public DataExtractor<class Type>
+class CalulateData : DataExtractor
 
     // Notes for functions: 
      // are these getters or setters? 
@@ -125,7 +158,7 @@ class CalulateData : public DataExtractor<class Type>
     { 
 
         // For each store in Items item, compare cheapest price to price a, price b, etc. 
-        for (auto element : vector_name) {
+        for (auto const element : vector_name) {
             
             for (auto innerElement : vector_name[element].store)
             {
@@ -157,15 +190,66 @@ class CalulateData : public DataExtractor<class Type>
 
         
     };
-    // Destin Mingledoff
+    
+
+    // Andrew Majewski
     void FindMostexpensive()
     {
 
 	};
-    // Silas Johnson
-    void TBA()
-    {
 
+    // Silas Johnson
+    // The mean is just the avarge of all the prices so in this instance it would the ((Kprice Mprice Wprice) / 3 = Mean).
+    double Mean(int item)
+    {
+        int mean = 0;
+        int count = 0;
+        for (auto item : items)
+        {
+            for (auto i : item.Stores)
+            {
+                int mean = mean + i.price;
+                count++;
+            }
+        }
+        mean = mean / count;
+        return mean;
+    }
+    //Silas Johnson
+    // double variance = (pow(price1 - mean, 2) + pow(price2 - mean, 2) + pow(price3 - mean, 2)) / 3.0;
+    double variance(int mean)
+    {
+		int variance = 0;
+		int count = 0;
+		for (auto item : items)
+		{
+			for (auto i : item.Stores)
+			{
+                variance = pow(i.price - mean, 2);
+                count++;
+			}
+		}
+		variance = variance / count;
+		return variance;
+    }
+
+    // Silas Johnson
+    // 
+    void StandardDeviaion(int variance)
+    {
+        variance = variance / 
+        double std_dev = 0
+		int count = 0;
+		for (auto item : items)
+		{
+			for (auto i : item.Stores)
+			{
+				int variance = pow(i.price - mean, 2);
+				count++;
+			}
+		}
+		variance = variance / count;
+		return std_dev;
     };
 
     // Destin Mingledoff
@@ -175,42 +259,29 @@ class CalulateData : public DataExtractor<class Type>
     {
         // contianers
         // For every item and the container 'items'
-        for (const auto& item : items) {
+        for (auto& an_Item : items) {
             // initiaizesthe best price to the first store's price and pair that stores name in parrallel to that price as seen below:
-            double bestPrice = item.stores[0].price;
-            string bestStoreName = item.stores[0].name;
+            double bestPrice = an_Item.Stores[0].price;
+            string bestStoreName = an_Item.Stores[0].name;
 
             // For the item that is currently parsing: For every store available, do the below:
-            for (const auto& store : item.stores) 
+            for (const auto& a_Store : an_Item.Stores)
             {
                 // For the Store x that sells this item, see if it has a better price than the previous store. 
                 // If the price is cheaper hence '<' make it the new best price.
-                if (store.price < bestPrice)
+                if (a_Store.price < bestPrice)
                 {
 
-                    bestPrice = store.price;
-                   // Removd this, this could technically be here, but it would make the function do MORE than 1 thing, its suppose to get the best price, not the best store name.
-                    // bestStoreName = store.name;
+                    // sets old best price value to the price of the current store's price for the currently loaded item if it is the cheaper than the prior value that was inputed before. 
+                    bestPrice = a_Store.price;
+                 
 
 
 
                 }
             }
-
-            //!!!Issue: We will need to store the best price for this current item somewhere before it is de-allocated from memory:
-            /*
-            
-            Scenario a)  New Struct Variable to Hold Best Price = bestPrice; 
-            -> moves onto next item or out of loop and de-allocates current items best price variable. This would be the best if we do not return a value and it will keep us from having to return an stl container
-            or  send in a value throgh the parameters. 
-
-            Scenario b) 
-            returns bestPrice value.
-            
-            */
-
-            // going with scenario a)
-            item.bestPriceFound = bestPrice;
+            // populates modifiable iValue (current struct element of items) value bestPriceFound with the found best/updated best price. 
+            an_Item.bestPriceFound = bestPrice;
         }
 
         // We now have the best price, now, are we going to return it or have 
@@ -220,18 +291,20 @@ class CalulateData : public DataExtractor<class Type>
     void findBestStore() // Wrote as a setter.
     {
         // Initialize the best store to the first store in the item's list
-        string bestStoreName = item.stores[0].name;
-        double bestPrice = item.stores[0].price;
+        string bestStoreName = Items.stores.price;
+        double bestPrice = Items.stores.;
+
+        // sort hightest to lowest
 
         // Iterate over all the stores for the item and find the one with the lowest price
-        for (const auto& store : item.stores) {
+        for (const auto& store : Items::stores) {
             if (store.price < bestPrice) {
                 bestPrice = store.price;
                 bestStoreName = store.name;
             }
         }
 
-        item.BestStoreName = bestStoreName;
+        items.BestStoreName = bestStoreName;
     
     
     }
@@ -241,10 +314,49 @@ class CalulateData : public DataExtractor<class Type>
     // Destin Mingledoff
     void FindBestRank() // what 3 items are the cheapest (1st, 2nd, 3rd), and which stores are selling them at cheapest value.
     {
+
+        string first_cheapest_item;
+        string second_cheapest_item;
+        string third_cheapest_item;
+        string loggedInfo;
+
+        for (const auto& item : ::items) 
+        {
+            item.bestPriceFound 
+            // if current new item.bestprice < last item.bestprice
+                // make last item.best price first
+                // make
+            
+        }
+
+        loggedInfo = "[FindBestPrice] + ";
+        OutPutFile();
         
     };
 
-
+    /*
+	struct Items
+{
+	struct Store
+	{
+		string name;
+		double price;
+		string rank;
+	};
+	int id;
+	string itemName;
+	vector<Store> Stores;
+	string bestStoreValue;
+	double bestPriceFound = 0; 
+	string bestStoreFound = "";
+}
+struct uniqueStore
+{
+	string name;
+	int score;
+	bool StorePlaceRank;
+};
+    */
 
 
     // Andrew Majewski
@@ -255,17 +367,21 @@ class CalulateData : public DataExtractor<class Type>
         const int SECOND_SCORE = 20;
 		const int FIRST_SCORE = 30;
 
-
-
-        for (const auto& items.store.name)
+        for (auto& i : items)
         {
-            best_price = store.price;
-            best_store_name = store.name;
-
-
+            for ()
         }
-
     };
+
+    // Andrew Majewski
+    virtual void OutPutFile(string logLine) // sends data to log.txt prettily  logging data as it goes.
+    {
+        // write the code to put a line into the log.txt file.
+    }
+
+
+
+
 };
 
 // Andrew Majewski 
@@ -276,7 +392,7 @@ class DisplayData : CalulateData
 
     }
 
-    void OutPutFile()
+    void OutPutFile(string logLine)
     {
 
     }
