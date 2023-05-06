b369f9c5f3536ce883dc676ae16d2de75e2c18ac
Ultimate Raider BTW <dezraymartin@gmail.com>
2023-05-06 16:31:14 -0400

    yes

--- Project1/StorePriceApp.cpp
+++ Project1/StorePriceApp.cpp
@@ -8,9 +8,36 @@
 
     This program will be a store compare application that will take in a JSON file to obtain prices and
     caclulate which is the cheapest or best value among the stores.
-*/
 
-// Hello I am a teapot
+    Check List
+
+    PC = Planed Check = It is planed but not implamented
+    CC = Compleated Check = We meet the requirement
+    ... = Uncertain of the implantation
+
+	1. At least 3 user defined C++ class including - 15pts:                                         (CC)( DataExtractor, CalulateData, DisplayData)
+        a. Class inheritance w/polymorphism - 10pts.                                                    (PC)(CalulateData to DisplayData Logging Data)
+        b. Class composition - 5pts.                                                                    (CC)(Classs are structured as such)
+        c. At least 2 overloaded operators - 10pts.                                                     (...)(Look below for potintal ways to make this work.)
+    2. The defining and use of at least one user-defined template class - 15pts.                    (CC)(All the classes are templates Muahahah!!!)
+    3. At least one user-defined function that performs searching - 10pts.                          (PC)(We will use a function to call up data on user user request to find data.)
+    4. At least one user-defined function that performs sorting - 5pts.                             (PC)(Check: In DisplayData Class there is a sortbeststore.)
+    5. Use of at least one Standard Template Library container                                      (CC)(We are using Vector)
+        (e.g., vector, queue) which is used in an iteration of its contents - 5pst.                 
+    6. Dynamic memory allocation (and deletion) in some form - 5pts.                                (...)(Need use pointers)
+    7. At least one use of exception-handling (does not have to be a custom class) - 5pts.          (CC)(Try and catach statemnt in json parse.)
+    8. Reading and writing of data to a file - 5pts.                                                (PC)(Using json file and writing a log)
+    9. I/O to/from the console - 5pts.                                                              (PC)(Will write data and calucation finding on console)
+    10. Comments that convince the instructor you understand the code you wrote - 5pts.             (CC)
+    11. GitHub repo that contains all relevant source code and text files - 10pts.                  (CC)
+
+	At least 2 overloaded operators Expanded: So This could be used for the calulate data so we can overload tha / operator to add up all the prices from an itmes and avratge it out.
+    we can also use the + operator to add up all the prices to geather in an item.
+
+    ++ get average
+    << Diaplay data (Could be useful for pretty print.)
+
+*/
 
 #include <iostream>
 #include <string>
@@ -50,7 +77,7 @@ struct Items
 
 // Difference between struct store and unique store.
 // - 3 items, walmart, kroger, and whatever the other one was.
-// - the best store ranking is dependent on whichever has the best price value overall for all accountable items (omitting duplicates).. 
+// - the best store ranking is dependent on whichever has the best price value overall for all accountable items (omitting duplicate names).. 
 // - Will be 
 struct uniqueStore
 {
@@ -61,21 +88,14 @@ struct uniqueStore
 
 
 // Silas Johnson
-// template <class T>
+template <typename T>
  class DataExtractor
 {
-public:
-    vector<Items> items;
-    vector<uniqueStore> uStores;
+public
+	vector<T> items;
+	vector<uniqueStore> uStores;
 
-    DataExtractor()
-    {
-        items;
-        uStores;
-    }
-
-
-protected:
+private:
     json j;
    
 	void getJSONData(string filename)
@@ -84,21 +104,25 @@ protected:
 		string data;
 		if (inFile.is_open())
 		{
-			while (getline(inFile, data))
-			{
-				j += json::parse(data);
+            try
+            {
+		        while (getline(inFile, data))
+		        {
+			        j += json::parse(data);
+		        }
 			}
+            catch (const exception& nf)
+            {
+                cerr << "Error while parsing Json data: " << nf.what() << endl;
+            }
 		}
 	};
-
-    
 
 	void parseJSONData()
 	{
         //items[0].Stores
 		for (auto& jei : j)
 		{
-            
 			Items i;
 			i.id = jei["id"];
 			i.itemName = jei["item"];
@@ -114,46 +138,65 @@ protected:
 
 	};
 
-    
-
     // Andrew Majewksi
     // This will extract the uninue names from items. 
-    void extractUniqueStore()
-    {
-        unordered_set<string> uniqueStoreNames;
+	void extractUniqueStore()
+	{
+		uniqueStore uniqueStoreObj;
+		std::vector<std::string> uniqueStoreNames;
 
-        for (const auto& j["stores"] : store)
-        {
-            const std::string& storeName = item.store.name;
+		for (const auto& item : items)
+		{
+			const std::vector<Items::Store>& stores = item.Stores;
 
-            // Check if the store name is already in the set
-            if (uniqueStoreNames.find(storeName) == uniqueStoreNames.end())
-            {
-                // Add the store name to the set
-                uniqueStoreNames.insert(storeName);
+			for (const auto& store : stores)
+			{
+				const std::string& storeName = store.name;
 
-                // Create a new uniqueStore object
-                uniqueStore uniqueStoreObj;
-                uniqueStoreObj.name = storeName;
-                uniqueStoreObj.score = 0;
-                uniqueStoreObj.StorePlaceRank = "";
+				// Check if the store name is already in the vector
+				auto it = std::find(uniqueStoreNames.begin(), uniqueStoreNames.end(), storeName);
+				if (it == uniqueStoreNames.end())
+				{
+					// Add the store name to the vector
+					uniqueStoreNames.push_back(storeName);
 
-                // Add the uniqueStore object to the stores vector
-                stores.push_back(uniqueStoreObj);
-            }
-        }
-    }
+					// Create a new uniqueStore object
+					uniqueStoreObj.name = storeName;
+					uniqueStoreObj.score = 0;
+					uniqueStoreObj.StorePlaceRank = false;
+
+					// Add the uniqueStore object to the uStores vector
+					uStores.push_back(uniqueStoreObj);
+				}
+			}
+		}
+	}
+
+public:
+	
+
+	void resolveJOSN(string fileName)
+	{
+		getJSONData(fileName);
+		parseJSONData();
+	}
+
 
 };
 
-class CalulateData : DataExtractor
-
-    // Notes for functions: 
-     // are these getters or setters? 
+ template <typename T>
+class CalculateData :  public DataExtractor<T>
 {
 
 
+	// Notes for functions: 
+	 // are these getters or setters? 
+
     // Destin Mingledoff
+
+    public 
+
+
     void FindDifference()
     { 
 
@@ -329,7 +372,7 @@ class CalulateData : DataExtractor
             
         }
 
-        loggedInfo = "[FindBestPrice] + ";
+        loggedInfo = "[FindBestPrice] +  ";
         OutPutFile();
         
     };
@@ -363,13 +406,33 @@ struct uniqueStore
     // can change price to best price. 
     void FindBestOverallStore()
     {
-        const int THRID_SCORE = 10;
-        const int SECOND_SCORE = 20;
 		const int FIRST_SCORE = 30;
+		const int SECOND_SCORE = 20;
+        const int THRID_SCORE = 10;
+        
+		
 
-        for (auto& i : items)
+        for (int i = 0; i < items.size(); i++;)
         {
-            for ()
+            for (int s = 0; s < items[i].Stores.size(); s++;)
+            {
+                if (items[i].Stores[s].rank == "1st")
+                {
+                    items[i].Stores[s].score = FIRST_SCORE;
+                }
+                else if (items[i].Stores[s].rank == "2nd")
+                {
+					items[i].Stores[s].score = SECOND_SCORE;
+                }
+				else if (items[i].Stores[s].rank == "3rd")
+				{
+					items[i].Stores[s].score = THRID_SCORE;
+				}
+                else
+                {
+					cerr << "Error could not find stated rank: " << endl;
+                }
+            }
         }
     };
 
@@ -385,8 +448,12 @@ struct uniqueStore
 };
 
 // Andrew Majewski 
-class DisplayData : CalulateData
-{
+template <typename T>
+class DisplayData : public CalculateData<T>
+{   
+	// Will sort the best store on rank and score.
+    
+
     void ConsoleDisplay()
     {
 
@@ -396,11 +463,19 @@ class DisplayData : CalulateData
     {
 
     }
+    // This will be our find function
+    void findById()
+    {
+
+    }
 
 };
 
 // Destin Mingledoff
 int main()
 {
-    cout << "Hello World!\n";
+    DisplayData<Items> ddi;
+    ddi.resolveJOSN(""); // JSON full name needs to be placed.
+    
+    // need to call function to parse extracrt data, but it is protectected. 
 }
